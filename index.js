852834b424c0fa2beb01f6925b58cb62405d2e92
AlexPortoNascimento <alexandreporto@id.uff.br>
2025-04-23 15:52:13 -0300

    Fazenndo listagem com Crud simples

--- index.js
+++ index.js
@@ -1,35 +1,26 @@
-//abertura do server express com require
 const express = require("express");
 const server = express();
 
-//http://localholst:3000/hello?nome=Alexandre?idade=27
-//Query params (querry = consulta) = ?nome=Alexandre&idade=27
-//os parametros de consulta não são obrigatórios, entao, se nenhum parametro for oferecido, ele entrará na rota, e usará o undefined
+server.use(express.json());
 
-//utilizando o servidor para enviar a mensagem de hello world em formato json
-server.get("/hello", (req, res) => {
-    const { nome, idade } = req.query;
+let customers = [
+    {id: 1, name: "Dev Samurai", site: "http://devsamurai.com.br"},
+    {id: 2, name: "Google", site: "http://google.com.br"},
+    {id: 3, name: "UOL", site: "http://uol.com.br"},
+    {id: 4, name: "Tibia", site: "http://tibia.com"},
 
+];
 
-    return res.json({
-        title: "Hello World",
-        message: `Olá ${nome}, tudo bem?`,
-        idade: idade,
-    })
-});
-
-//http://localhost:3000/hello/Alexandre
-//Route params = /hello/:nome
-//parametros de rota são obrigatórios, ou seja, é requerido que tenha o /alexandre para que essa rota seja usada
-
-server.get("/hello/:nome", (req, res) => {
-    const nome = req.params.nome;
-
-    return res.json({
-        title: "Hello World",
-        message: `Olá ${nome}, tudo bem?`
-    })
+server.get("/customers", (req,res) => {
+    return res.json(customers);
 })
 
-//determinando a porta a ser utilizada pelo servidor
-server.listen(3000);
+server.get("/customers/:id", (req,res) => {
+    const id = parseInt(req.params.id);
+    const customer = customers.find(item => item.id === id);
+    const status = customer ? 200 : 404; 
+
+    return res.status(status).json(customer);
+})
+
+server.listen(3000)
